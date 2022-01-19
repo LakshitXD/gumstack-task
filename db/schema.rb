959642155a052f1f9d4a8df79bb8d9893aec3fae
LakshitXD <lakshitk6666@gmail.com>
2022-01-20 01:37:51 +0530

    changed db to sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2022_01_18_051451) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "cities", force: :cascade do |t|
     t.string "city_name"
     t.string "country_name"
