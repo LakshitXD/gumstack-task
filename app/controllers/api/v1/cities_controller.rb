32b3fbb52ae84ca7a44d41a4e70bbcb16a2b6b4b
LakshitXD <lakshitk6666@gmail.com>
2022-01-20 13:05:35 +0530

    trying cors added before_action

--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -5,6 +5,14 @@ module Api
     # City API
     class CitiesController < ApplicationController
       include JSONAPI::ActsAsResourceController
+      before_action :cors_headers
+
+      def cors_headers
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+        headers['Access-Control-Request-Method'] = '*'
+        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+      end
     end
   end
 end
