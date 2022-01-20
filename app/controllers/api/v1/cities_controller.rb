5eb1ed5bbf8aa6a6e31a5655b5b9fa612a6fdb0d
LakshitXD <lakshitk6666@gmail.com>
2022-01-20 13:09:05 +0530

    trying cors added 2

--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -8,10 +8,11 @@ module Api
       before_action :cors_headers
 
       def cors_headers
-        headers['Access-Control-Allow-Origin'] = '*'
-        headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-        headers['Access-Control-Request-Method'] = '*'
-        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        byebug
+        request.headers['Access-Control-Allow-Origin'] = '*'
+        request.headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+        request.headers['Access-Control-Request-Method'] = '*'
+        request.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
       end
     end
   end
